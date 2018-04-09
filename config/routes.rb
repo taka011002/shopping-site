b3e1c3eebf090909d22d624174df816ef9f6b23b
Piece123 <taka.rock.i4@gmail.com>
2018-04-09 20:33:30 +0900

    A1-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
+  root'static_pages#home'
+  get 'login' => 'sessions#new'
+  get 'signup' => 'users#new'
 
   get 'users/index'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
