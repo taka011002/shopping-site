bda57bcc0a9052d7508af93b77c66d4abb27d530
Piece123 <taka.rock.i4@gmail.com>
2018-04-12 15:26:54 +0900

    has_many 関連付け前

--- db/migrate/20180412050226_create_cart_items.rb
+++ db/migrate/20180412050226_create_cart_items.rb
@@ -1,9 +1,9 @@
 class CreateCartItems < ActiveRecord::Migration[5.1]
   def change
     create_table :cart_items do |t|
-      t.intger :user_id
-      t.intger :product_id
-      t.intger :quantity
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :quantity
 
       t.timestamps
     end
